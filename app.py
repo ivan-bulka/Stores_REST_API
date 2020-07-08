5ea9a1b595d16a111081c89ba68d121c5c0c263c
Ivan Baaton <ivan.bulka0107@gmail.com>
2020-07-08 10:37:52 +0300

    Postgre integration

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os  # CHANGES
+
 from flask import Flask
 from flask_restful import  Api
 from flask_jwt import JWT
@@ -6,15 +8,14 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList 
-# Delete the line here 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL', 'sqlite:///data.db') # 'DATABASE_URL' - environmental variable,
+# 'sqlite:///data.db' - default value. If we do not find 'DATABASE_URL', we use 'sqlite:///data.db'. It helps to cover 2 scenarios:
+# working on heroku and working on the local machine 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.secret_key = 'jose'
 api = Api(app)
-
-# Delete the decorator here 
 
 jwt = JWT(app, authenticate, identity) 
 
